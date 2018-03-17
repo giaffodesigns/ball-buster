3e17f4ca04c47d2df0036ebafd74e6db4fd46af7
joshjaysalazar <joshjaysalazar@gmail.com>
2018-03-16 20:56:18 -0500

    Update to main script

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 import sys
 import math
 from pygame.locals import *
@@ -31,9 +30,9 @@ def main():
     allsprites.add(paddle)
 
     # Create the brick
-    brick = Brick()
+    '''brick = Brick()
     allsprites.add(brick)
-    bricks.add(brick)
+    bricks.add(brick)'''
 
     game_over = False
 
@@ -46,36 +45,48 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[K_LEFT]:
-            paddle.x -= paddle.speed
+            paddle.pos -= paddle.speed
         if keys_pressed[K_RIGHT]:
-            paddle.x += paddle.speed
+            paddle.pos += paddle.speed
 
         # Main Surface Drawing Functions
         DISPLAYSURF.fill(DARKBLUE)
         allsprites.draw(DISPLAYSURF)
 
-        pygame.display.update()                                                 # draw to surface
+        pygame.display.flip()                                                   # draw to surface
         FPSCLOCK.tick(FPS)
 
 class Paddle(pygame.sprite.Sprite):
+    speed = 5.0
+
     def __init__(self):
         super().__init__()
 
         self.image = pygame.image.load('paddle.png')
-        self.rect = self.sprite.get_rect()
+        self.rect = self.image.get_rect()
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
 
-        self.rect.x = WINDOWWIDTH / 2
+        self.pos = (WINDOWWIDTH / 2) + (self.width / 2)
         self.rect.y = WINDOWHEIGHT - 50
-        self.speed = 5
+
+    def update(self):
+        self.rect.x = self.pos
+        # Make sure the paddle doesn't move off the screen
+        if self.rect.x > WINDOWWIDTH - self.width:
+            self.rect.x = WINDOWWIDTH - self.width
+        elif self.rect.x < WINDOWWIDTH:
+            self.rect.x = WINDOWWIDTH
+
 
 class Brick(pygame.sprite.Sprite):
+    speed = 5.0
 
 
     def __init__(self):
         super().__init__()
 
         self.image = pygame.image.load('brick.png')                       # not set as surface
-        self.speed = 5
 
         self.direction = 200
 
