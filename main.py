d92c6ad44426f0b9b2a71b37bf07e5d723758293
joshjaysalazar <joshjaysalazar@gmail.com>
2018-03-16 20:34:29 -0500

    More basic code updates

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 import random
 import sys
+import math
 from pygame.locals import *
 
-FPS = 30
+FPS = 60
 WINDOWWIDTH = 800
 WINDOWHEIGHT = 600
 
@@ -13,12 +14,28 @@ DARKBLUE = (0, 61, 102)
 def main():
     global FPSCLOCK, DISPLAYSURF
 
-    paddle = Paddle()
-
     pygame.init()
     FPSCLOCK = pygame.time.Clock()                                              # come back to understand classes
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('Ball Breaker')
+
+    FONT = pygame.font.Font(None, 24)
+
+    # Create sprite lists
+    balls = pygame.sprite.Group()
+    bricks = pygame.sprite.Group()
+    allsprites = pygame.sprite.Group()
+
+    # Create the player paddle
+    paddle = Paddle()
+    allsprites.add(paddle)
+
+    # Create the brick
+    brick = Brick()
+    allsprites.add(brick)
+    bricks.add(brick)
+
+    game_over = False
 
     while True:
         # Event Loop
@@ -35,17 +52,35 @@ def main():
 
         # Main Surface Drawing Functions
         DISPLAYSURF.fill(DARKBLUE)
-        DISPLAYSURF.blit(paddle.sprite, (paddle.x, paddle.y))                                        # add to DISPLAYSURF surface
+        allsprites.draw(DISPLAYSURF)
 
-        pygame.display.update()                                                     # draw to surface
+        pygame.display.update()                                                 # draw to surface
         FPSCLOCK.tick(FPS)
 
-class Paddle:
+class Paddle(pygame.sprite.Sprite):
     def __init__(self):
-        self.sprite = pygame.image.load('paddle.png')
+        super().__init__()
+
+        self.image = pygame.image.load('paddle.png')
+        self.rect = self.sprite.get_rect()
+
+        self.rect.x = WINDOWWIDTH / 2
+        self.rect.y = WINDOWHEIGHT - 50
+        self.speed = 5
+
+class Brick(pygame.sprite.Sprite):
+
+
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load('brick.png')                       # not set as surface
+        self.speed = 5
+
+        self.direction = 200
+
         self.x = WINDOWWIDTH / 2
-        self.y = WINDOWHEIGHT - 50
-        self.speed = 2
+        self.y = WINDOWHEIGHT - 100
 
 if __name__ == '__main__':
     main()
