e0271e777d8c28a3554636181bd0f915763a9a5c
Jason <jason@giaffodesigns.com>
2018-04-25 13:44:46 -0500

    Updated brick class

--- bricks.py
+++ bricks.py
@@ -24,39 +24,31 @@ brick_image = [
 
 brick_props = [
     #type, color, hits
-    [0, WHITE, 1],
-    [0, RED, 1],
-    [0, GREEN, 1],
-    [0, BLUE, 1],
-    [0, CYAN, 1],
-    [0, MAGENTA, 1],
-    [0, YELLOW, 1],
-    [0, BLACK, 1],
-    [0, GREY, 1],
+    [0, WHITE, 1],          #0
+    [0, RED, 1],            #1
+    [0, GREEN, 1],          #2
+    [0, BLUE, 1],           #3
+    [0, CYAN, 1],           #4
+    [0, MAGENTA, 1],        #5
+    [0, YELLOW, 1],         #6
+    [0, BLACK, 1],          #7
+    [0, GREY, 1],           #8
 ]
 
 
 class Brick(pygame.sprite.Sprite):
-    def __init__(self, , x_pos, y_pos, brick_image):
+    # ex: brick = Brick({type:}0, {x:}50, {y:}20)
+    def __init__(self, type, x_pos, y_pos, brick_image=brick_image, brick_props=brick_props):
         super().__init__()
 
-        self.image = pygame.Surface.copy(brick_image)
-        self.image.fill(color, special_flags=BLEND_MULT)
+        props = brick_props[type]   #quick access to specific properties list
+        #props = [type, color, hits]
+
+        self.image = pygame.Surface.copy(brick_image[props[0]])
+        self.image.fill(props[1], special_flags=BLEND_MULT)
         self.rect = self.image.get_rect()
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
         self.rect.x = x_pos
         self.rect.y = y_pos
-
-
-class Brick_Call(Brick):
-    def __init__(shape, type, x_pos, y_pos):
-
-
-
-
-
-
-for info in list:
-    Brick_Call(shape, type, x_pos, y_pos)
