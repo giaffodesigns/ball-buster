15c3c7775137ea05d54ed752e3dc1bf7453955f9
joshjaysalazar <joshjaysalazar@gmail.com>
2018-04-25 13:57:46 -0500

    Update to main.py

--- main.py
+++ main.py
@@ -4,22 +4,11 @@ import math
 import random
 from pygame.locals import *
 from bricks import *
+from colors import *
 
 FPS = 60
 WINDOWWIDTH = 800
 WINDOWHEIGHT = 600
-
-# Colors
-RED      = (255,   0,   0)
-GREEN    = (  0, 255,   0)
-BLUE     = (  0,   0, 255)
-YELLOW   = (255, 255,   0)
-MAGENTA  = (255,   0, 255)
-CYAN     = (  0, 255, 255)
-BLACK    = (  0,   0,   0)
-GRAY     = (128, 128, 128)
-WHITE    = (255, 255, 255)
-DARKBLUE = (  0,   0, 128)
 
 # Defaults
 PADDLEMAXSPEED = 5
